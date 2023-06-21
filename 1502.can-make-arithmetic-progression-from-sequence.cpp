01f83472e6eae34e1a28d18ed6a35f10a50d4604
Kavya Reddy <nilipalli.kavya2020@vitstudent.ac.in>
2023-06-21 21:32:12 +0530

    commit

---
+++ 1502.can-make-arithmetic-progression-from-sequence.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1502 lang=cpp
+ *
+ * [1502] Can Make Arithmetic Progression From Sequence
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int diff=arr[1]-arr[0];
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]-arr[i-1] != diff){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+// @lc code=end
+
