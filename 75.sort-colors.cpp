c98103aa466ac452fbca676fcc69f9e6c5af3484
Kavya Reddy <nilipalli.kavya2020@vitstudent.ac.in>
2023-06-25 22:05:40 +0530

    commit

---
+++ 75.sort-colors.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=75 lang=cpp
+ *
+ * [75] Sort Colors
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int low=0,mid=0,high=n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                int temp=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
+                high--;
+            }
+        }
+    }
+};
+// @lc code=end
+
