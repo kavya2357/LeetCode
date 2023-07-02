194b964670bf8596abf7da845115e77d4e4c8b77
Kavya Reddy <nilipalli.kavya2020@vitstudent.ac.in>
2023-07-02 12:59:37 +0530

    commit

---
+++ 53.maximum-subarray.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=53 lang=cpp
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0,max_sum=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            max_sum=max(sum,max_sum);
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return max_sum;
+    }
+};
+// @lc code=end
+
