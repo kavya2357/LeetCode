8042e2ff3e643131bdad5af48bbf457a962975f2
Kavya Reddy <nilipalli.kavya2020@vitstudent.ac.in>
2023-07-06 18:08:22 +0530

    commit

---
+++ 48.rotate-image.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=48 lang=cpp
+ *
+ * [48] Rotate Image
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m/2;j++){
+                swap(matrix[i][j],matrix[i][m-j-1]);
+            }
+        }
+    }
+};
+// @lc code=end
+
