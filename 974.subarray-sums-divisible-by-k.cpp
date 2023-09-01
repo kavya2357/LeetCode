6fef7861644716982501ee395f33d62631ec7179
Kavya Reddy <nilipalli.kavya2020@vitstudent.ac.in>
2023-09-01 19:20:13 +0530

    commit

--- 974.subarray-sums-divisible-by-k.cpp
+++ 974.subarray-sums-divisible-by-k.cpp
@@ -9,20 +9,20 @@ class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
         map<int,int> mp;
-        int prefix_sum=0;
         mp[0]=1;
-        int ans=0;
+        int sum=0,rem=0,ans=0;
         for(int i=0;i<nums.size();i++){
-            prefix_sum+=nums[i];
-            if(prefix_sum<0){
-                prefix_sum+= ceil(abs(prefix_sum)*1.0/k)*k;
+            sum+=nums[i];
+            rem=sum%k;
+            if(rem<0){
+                rem+=k;
             }
-            prefix_sum%=k;
-            if(mp.find(prefix_sum)!=mp.end()){
-                ans+=mp[prefix_sum];
-
+            if(mp.find(rem)==mp.end()){
+                mp[rem]=1;
+            }else{
+                ans+=mp[rem];
+                mp[rem]++;
             }
-            mp[prefix_sum]++;
         }
         return ans;
     }
