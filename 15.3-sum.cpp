3c24492d2417ccf3c0206818be2a20c9175e513b
Kavya Reddy <nilipalli.kavya2020@vitstudent.ac.in>
2023-09-19 19:52:13 +0530

    commit

--- 15.3-sum.cpp
+++ 15.3-sum.cpp
@@ -8,29 +8,30 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int target = 0;
-        sort(nums.begin(), nums.end());
-        set<vector<int>> s;
-        vector<vector<int>> output;
-        for (int i = 0; i < nums.size(); i++){
-            int j = i + 1;
-            int k = nums.size() - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == target) {
-                    s.insert({nums[i], nums[j], nums[k]});
+        vector<vector<int>>res;
+        sort(nums.begin(),nums.end());
+        set<vector<int>>s;
+        for(int i=0;i<nums.size()-2;i++){
+            int j=i+1;
+            int k=nums.size()-1;
+            while(j<k){
+                int ans=nums[i]+nums[j]+nums[k];
+                if(ans==0){
+                    s.insert({nums[i],nums[j],nums[k]});
                     j++;
                     k--;
-                } else if (sum < target) {
-                    j++;
-                } else {
+                }else if(ans>0){
                     k--;
+                }
+                else{
+                    j++;
                 }
             }
         }
-        for(auto triplets : s)
-            output.push_back(triplets);
-        return output;
+        for(auto it:s){
+            res.push_back(it);
+        }
+        return res;
     }
 };
 // @lc code=end
