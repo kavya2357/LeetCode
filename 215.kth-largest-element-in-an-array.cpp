78ecf610b02a7cdf8604110e04398ba03ccc2b61
Kavya Reddy <nilipalli.kavya2020@vitstudent.ac.in>
2023-09-20 17:57:52 +0530

    commit

---
+++ 215.kth-largest-element-in-an-array.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=215 lang=cpp
+ *
+ * [215] Kth Largest Element in an Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        return nums[n-k];
+    }
+};
+// @lc code=end
+
