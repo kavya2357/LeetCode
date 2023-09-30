00c29e58e2d5c3e9511274d7bf7a4f2cbe4e33a0
Kavya Reddy <nilipalli.kavya2020@vitstudent.ac.in>
2023-09-30 14:34:15 +0530

    commit

--- 289.game-of-life.cpp
+++ 289.game-of-life.cpp
@@ -8,44 +8,30 @@
 class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
-        int rows = board.size();
-        int cols = board[0].size();
-
-        //copying the board to 2-D vector copy
-        vector<vector<int>> copy(board);
-
-        for(int i=0; i<rows; i++)
-        {
-            for(int j=0; j<cols; j++)
-            {
-                //for counting live neighbors of the element
-                int liveNeighbors = 0; 
-                
-                //iterating through all the neighbors of the element (inside the board and outside the board)
-                for(int m=i-1; m<i+2; m++)
-                {
-                    for(int n=j-1; n<j+2; n++)
-                    {
-                        //we will skip the iteration when element itself is encountered
-                        if(m==i && n==j)
-                            continue;
-
-                        //checking the bounds, if indices are in range, we will check for liveNeighbors
-                        if(m>=0 && m<rows && n>=0 && n<cols && copy[m][n] == 1)
-                            liveNeighbors++;
+        int n = board.size();
+        int m = board[0].size();
+        
+        int dx[]={1,1,0,-1,-1,-1,0,1};
+        int dy[]={0,1,1,1,0,-1,-1,-1};
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int lc=0;
+                for(int k=0;k<8;k++){
+                    if((i+dx[k]>=0 && i+dx[k]<n && j+dy[k]>=0 && j+dy[k]<m) && abs(board[i+dx[k]][j+dy[k]])==1){
+                        lc++;
                     }
                 }
-                
-                //for live cells, if liveNeighbors are less than 2 or more than 3, cells will die and dead cells are already dead, so we need not check cell to be live or dead
-                if(liveNeighbors < 2 || liveNeighbors > 3)
-                    board[i][j] = 0;
-
-                //for dead cells, if liveNeighbors are exactly 3, it will live and live cell will also live for exactly 3 liveNeighbors
-                else if(liveNeighbors == 3)
-                    board[i][j] = 1;
-
-                //For live cells, if liveNeighbors are exactly 2, it will live but we need not update it as it is already live
-
+                if(board[i][j]==0 && lc==3){
+                        board[i][j]=2;
+                }
+                if(board[i][j]==1 && (lc<2 || lc>3)){
+                        board[i][j]=-1;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                board[i][j]=board[i][j]>0 ? 1 : 0;
             }
         }
     }
