8d864c285055066a2919bdbd6ef9ef647ea0f007
Kavya Reddy <nilipalli.kavya2020@vitstudent.ac.in>
2024-01-21 19:38:02 +0530

    commit

---
+++ 144.binary-tree-preorder-traversal.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=144 lang=cpp
+ *
+ * [144] Binary Tree Preorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>v;
+    void preorder(TreeNode *root){
+        if(root==NULL){
+            return;
+        }
+        v.push_back(root->val);
+        preorder(root->left);
+        preorder(root->right);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        preorder(root);
+        return v;
+    }
+};
+// @lc code=end
+
