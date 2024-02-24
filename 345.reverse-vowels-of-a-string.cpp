4e9a608c55f77626a2c79471f555d1166d3a4429
Kavya Reddy <nilipalli.kavya2020@vitstudent.ac.in>
2024-02-24 22:04:16 +0530

    commit

---
+++ 345.reverse-vowels-of-a-string.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=345 lang=cpp
+ *
+ * [345] Reverse Vowels of a String
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string reverseVowels(string s) {
+        string word = s;
+        int start = 0;
+        int end = s.length() - 1;
+        string vowels = "aeiouAEIOU";
+        
+        // Loop until the start pointer is no longer less than the end pointer.
+        while (start < end) {
+            // Move the start pointer towards the end until it points to a vowel.
+            while (start < end && vowels.find(word[start]) == string::npos) {
+                start++;
+            }
+            
+            // Move the end pointer towards the start until it points to a vowel.
+            while (start < end && vowels.find(word[end]) == string::npos) {
+                end--;
+            }
+            
+            // Swap the vowels found at the start and end positions.
+            swap(word[start], word[end]);
+            
+            // Move the pointers towards each other for the next iteration.
+            start++;
+            end--;
+        }
+        
+        // Return the modified string.
+        return word;
+    }
+};
+// @lc code=end
+
